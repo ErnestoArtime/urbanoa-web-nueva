ad5943500687140ab44f41f008fd70a261b4b146
Jorge <j.naredo@gentalia.es>
2026-09-07 16:29:15 -0400

    fix(OPSWebServicesAPI): rollback operating system to resolve server-side login malfunction

--- src/app/core/api/ops-client.constants.ts
+++ src/app/core/api/ops-client.constants.ts
@@ -4,5 +4,5 @@ export const OPS_OPERATING_SYSTEMS = {
   web: 3,
 } as const;
 
-export const OPS_OPERATING_SYSTEM = OPS_OPERATING_SYSTEMS.web;
+export const OPS_OPERATING_SYSTEM = OPS_OPERATING_SYSTEMS.android;
 export const OPS_APP_VERSION = '4.0.0';
